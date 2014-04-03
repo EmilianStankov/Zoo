8022b27d1d629c70d01d45a29254e882e25369ff
Emilian <emilian@emilian-ThinkPad-T61.(none)>
2014-04-03 15:03:11 +0300

    Outcome, Income and accomodate implemented

--- zoo.py
+++ zoo.py
@@ -1,4 +1,5 @@
 import sqlite3
+from class_animal import Animal
 
 
 class Zoo():
@@ -46,19 +47,40 @@ class Zoo():
         for line in cursor.execute(query, data):
             zoo_id = line[0]
         cursor.execute("""CREATE TABLE IF NOT EXISTS animals_in_zoo
-                          (zoo_id, species, name, status,
+                          (zoo_id, species, name, status, gender,
                            unique_animal_id INTEGER PRIMARY KEY AUTOINCREMENT)
                        """)
-        query = ("""INSERT INTO animals_in_zoo(zoo_id, species, name, status)
-                    VALUES(?, ?, ?, ?)""")
+        query = ("""INSERT INTO animals_in_zoo(zoo_id, species,
+                                               name, status, gender)
+                    VALUES(?, ?, ?, ?, ?)""")
         data = [zoo_id, self._animal.get_species(),
-                self._animal.get_name(), self.get_status()]
+                self._animal.get_name(), self._animal.get_status(),
+                self._animal.get_gender()]
+        cursor.execute(query, data)
         db.commit()
         db.close()
 
-    #def daily_outcome(self):
-    #    __outcome = 0
-        #for animal in self.animals
+    def daily_outcome(self):
+        __outcome = 0
+        db = sqlite3.connect("animals.db")
+        cursor = db.cursor()
+        for animal in self._animals:
+            __weight = animal.get_weight()
+            __species = animal.get_species()
+            query = ("""SELECT food_type, food_weight_ratio
+                        FROM animals WHERE species = ?""")
+            data = [__species]
+            for line in cursor.execute(query, data):
+                type_of_food = line[0]
+                food_weight_ratio = line[1]
+            if type_of_food == 'carnivore':
+                __outcome += 4 * __weight * food_weight_ratio
+            else:
+                __outcome += 2 * __weight * food_weight_ratio
+
+        return int(__outcome)
 
 
-zoo = Zoo(['Lion'], 10, 1000, 'Zoo')
+lion = Animal('lion', 2, 'Pesho', 'male', 100)
+zoo = Zoo([lion], 10, 1000, 'Zoo')
+zoo.daily_outcome()
